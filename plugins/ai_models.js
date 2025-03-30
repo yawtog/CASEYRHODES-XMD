0418c493b167d62269acb844f3fef8d10944dbb8
caseyweb <caseywebsthedonn25@gmail.com>
2025-03-30 19:00:50 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- plugins/ai_models.js
+++ plugins/ai_models.js
@@ -20,7 +20,7 @@ cmd({
         const text = q;  // Texte de la requête de l'utilisateur
         const encodedText = encodeURIComponent(text);  // S'assurer que le texte est encodé correctement
 
-        const url = `https://lance-frank-asta.onrender.com/api/gpt?q=${encodedText}`;
+        const url = `https://apis-keith.vercel.app/ai/gpt?q=${encodedText}`;
 
         console.log('Requesting URL:', url);  // Afficher l'URL pour vérifier
 
